61aced84d568002f5b94f737fc97c86445f83736
victorpraiz <ivictorpraiz@gmail.com>
2023-05-03 06:24:46 +0100

    Transaction model validations annotations

--- Expense Tracker/Models/Transaction.cs
+++ Expense Tracker/Models/Transaction.cs
@@ -7,13 +7,20 @@ namespace Expense_Tracker.Models
     {
         [Key]
         public int TransactionId { get; set; }
+        
+        [Range(1, int.MaxValue, ErrorMessage ="Amount should be greater than 0.")]
         public int Amount { get; set; }
         public string? Note { get; set; }
         public DateTime Date { get; set; } = DateTime.Now;
+        [Range(1, int.MaxValue, ErrorMessage ="Please select a category.")]
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
         public Category? Category { get; set; }
 
+
+
+
+        
         [NotMapped]
         public string CategoryTitleWithIcon
         {
